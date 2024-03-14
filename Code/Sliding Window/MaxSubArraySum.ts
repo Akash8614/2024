69d39cae65cd2ccc0e28fade175f48fe9ce12c9e
Akash Agarwal <1998akash28@gmail.com>
2024-03-14 18:47:13 +0530

    Insert refernce link for better understanding

--- Code/Sliding Window/MaxSubArraySum.ts
+++ Code/Sliding Window/MaxSubArraySum.ts
@@ -8,6 +8,8 @@ Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 
+For Reference: "https://dev.to/akhilpokle/maximum-sum-subarray-of-size-k-applying-sliding-window-pattern-59fa"
+
 ****************************************************************************************************************/
 
 const LargestSum: (arr: number[], windowSize: number) => number = (
